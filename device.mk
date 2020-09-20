b827fffab7333f2e12b0502bbed7b83353e3c0b3
Tim Zimmermann <tim@linux4.de>
2020-09-20 11:15:39 +0200

    gta4xlveu: configs: keylayout: Add config for spen
    * Fixes spen in landscape orientation
    
    Change-Id: Icf9ec6efb1e04d150320d2016ee687c8ff3da97e

--- device.mk
+++ device.mk
@@ -9,6 +9,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery.fstab:$(TARGET_COPY_OUT_RAMDISK)/fstab.default
 
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/idc/sec_e-pen.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/sec_e-pen.idc
+
 # Shipping level
 PRODUCT_SHIPPING_API_LEVEL := 30
 PRODUCT_EXTRA_VNDK_VERSIONS := $(PRODUCT_SHIPPING_API_LEVEL)
