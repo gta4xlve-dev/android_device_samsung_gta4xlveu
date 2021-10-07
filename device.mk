5d533b57b9943477b2679d89275dd097f4c54178
Pavel Dubrova <pashadubrova@gmail.com>
2021-10-07 10:39:12 +0300

    gta4xlveu: Provide android.frameworks.sensorservice@1.0 on vendor
    Following the commit f19703e ("common-treble: Provide radio, keymaster
    and secure_element HAL on vendor"), starting with Android 12 most HAL
    libraries are not included in VNDK anymore - they need to be explicitly
    pulled in to `/vendor` for odm blobs to be allowed to access them:
    
        QCamera : <MCI><ERROR> mm_camera_load_shim_lib: 3306: dlopen failed
        with error dlopen failed: library
        "android.frameworks.sensorservice@1.0.so" not found: needed by
        /odm/lib/libmmcamera2_stats_modules.so in namespace (default)
    
    android.frameworks.sensorservice@1.0 is used by at least mm-camera
    framework.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>
    Change-Id: I72d9bcda1fdfd48c5fb9cb21666edd995ae4d9af

--- device.mk
+++ device.mk
@@ -61,6 +61,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0.vendor \
     android.hardware.camera.provider@2.5-service \
     android.hardware.camera.provider@2.4-impl
 
