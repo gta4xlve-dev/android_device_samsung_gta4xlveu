ca0d7e610da8908e404f17b6beebf4b073a797b8
danielml <daniel@danielml.dev>
2023-02-18 16:43:27 +0100

    gta4xlveu: Initial device tree
    * Kernel flags from atoll's CodeLinaro tree
    * Partitions flags manually read from the device
    * recovery.fstab taken directly from the device
    
    Change-Id: Ic8fbbeddf5edb291a06212374148fea36f6feeb5
    Signed-off-by: danielml <daniel@danielml.dev>

---
+++ device.mk
@@ -0,0 +1,14 @@
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# DTB
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/dtb.img:dtb.img
+
+# Fstab
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/recovery.fstab:$(TARGET_COPY_OUT_RAMDISK)/fstab.default
+
+# Shipping level
+PRODUCT_SHIPPING_API_LEVEL := 30
+PRODUCT_EXTRA_VNDK_VERSIONS := $(PRODUCT_SHIPPING_API_LEVEL)
