071887167a82ed52e4780b30e45a9ffd730aeec0
danielml <daniel@danielml.dev>
2023-02-26 15:03:10 +0100

    gta4xlveu: overlay: Replace reboot_bootloader with reboot_download
    * Samsung devices use the download mode instead of the fastboot mode
    
    Signed-off-by: danielml <daniel@danielml.dev>

--- device.mk
+++ device.mk
@@ -16,7 +16,8 @@ PRODUCT_COPY_FILES += \
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Properties
 PRODUCT_COPY_FILES += \
