8069f9713ec0b4ca6dd6012d3976a96e3c1426bb
danielml <daniel@danielml.dev>
2023-02-28 15:58:41 +0100

    gta4xlveu: Initial setup for included vendor
    Signed-off-by: danielml <daniel@danielml.dev>

--- device.mk
+++ device.mk
@@ -7,11 +7,17 @@ PRODUCT_COPY_FILES += \
 
 # Fstab
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery.fstab:$(TARGET_COPY_OUT_RAMDISK)/fstab.default
+    $(LOCAL_PATH)/recovery.fstab:$(TARGET_COPY_OUT_RAMDISK)/fstab.default \
+    $(LOCAL_PATH)/recovery.fstab:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.default
 
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/idc/sec_e-pen.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/sec_e-pen.idc
+
+# Properties
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/properties/odm_SM-P613.prop:$(TARGET_COPY_OUT_ODM)/etc/build_SM-P613.prop \
+    $(LOCAL_PATH)/properties/odm_SM-P619.prop:$(TARGET_COPY_OUT_ODM)/etc/build_SM-P619.prop
 
 # Rootdir
 PRODUCT_COPY_FILES += \
@@ -47,4 +53,6 @@ PRODUCT_COPY_FILES += \
 
 # Shipping level
 PRODUCT_SHIPPING_API_LEVEL := 30
-PRODUCT_EXTRA_VNDK_VERSIONS := $(PRODUCT_SHIPPING_API_LEVEL)
+
+# Proprietary components
+$(call inherit-product, vendor/samsung/gta4xlveu/gta4xlveu-vendor.mk)
