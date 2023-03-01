89d7abf5fef6e48fe1ed9d984151e6bc7afa699c
danielml <daniel@danielml.dev>
2023-03-01 08:10:46 +0100

    gta4xlveu: Import config.fs from CAF
    Change-Id: Ie3e1ce1d767b57200332b3f413fd7273301894e0
    Signed-off-by: danielml <daniel@danielml.dev>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,6 +22,9 @@ TARGET_NO_BOOTLOADER := true
 # Display
 TARGET_SCREEN_DENSITY := 240
 
+# Filesystem
+TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/config.fs
+
 # Kernel
 BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 androidboot.usbcontroller=a600000.dwc3 loop.max_part=7 printk.devkmsg=on firmware_class.path=/vendor/firmware_mnt/image
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
