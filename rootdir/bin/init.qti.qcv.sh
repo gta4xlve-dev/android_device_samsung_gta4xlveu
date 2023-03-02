0402d480cfd864ead322e23fb233aa27058d2eff
danielml <daniel@danielml.dev>
2023-03-02 10:35:54 +0100

    gta4xlveu: Initial vendor init scripts cleanup
    Change-Id: I0150a06da92bf988ee4eedfad33adb44c0bc446f
    Signed-off-by: danielml <daniel@danielml.dev>

--- rootdir/bin/init.qti.qcv.sh
+++ rootdir/bin/init.qti.qcv.sh
@@ -6,14 +6,4 @@
 #=============================================================================
 
 soc_id=`cat /sys/devices/soc0/soc_id` 2> /dev/null
-
-# Store soc_id in ro.vendor.qti.soc_id
 setprop ro.vendor.qti.soc_id $soc_id
-
-# For chipsets in QCV family, convert soc_id to soc_name
-# and store it in ro.vendor.qti.soc_name.
-if [ "$soc_id" -eq 415 ] || [ "$soc_id" -eq 439 ] || [ "$soc_id" -eq 456 ]; then
-    setprop ro.vendor.qti.soc_name lahaina
-elif [ "$soc_id" -eq 450 ]; then
-    setprop ro.vendor.qti.soc_name shima
-fi
