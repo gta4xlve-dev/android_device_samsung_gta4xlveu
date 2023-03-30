f48c4b653a123d533c566bdaa5139f7033c3c2fe
ad1tyas1ngh <adityasingh1768@gmail.com>
2023-03-31 01:15:22 +0530

    gta4xlveu: Fix EGL symlink
    Signed-off-by: ad1tyas1ngh <adityasingh1768@gmail.com>
    Change-Id: I63fe8a3f77dcc0f4c101df84c514dbc6cb6806d4

--- Android.mk
+++ Android.mk
@@ -70,14 +70,14 @@ EGL_SYMLINKS := $(TARGET_OUT_VENDOR)/lib/
 $(EGL_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	mkdir -p $@
 	$(hide) ln -sf egl/libEGL_adreno.so $@/libEGL_adreno.so
-	$(hide) ln -sf egl/libGLESv2_adreno $@/libGLESv2_adreno
+	$(hide) ln -sf egl/libGLESv2_adreno.so $@/libGLESv2_adreno.so
 
 
 EGL64_SYMLINKS := $(TARGET_OUT_VENDOR)/lib64/
 $(EGL64_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	mkdir -p $@
 	$(hide) ln -sf egl/libEGL_adreno.so $@/libEGL_adreno.so
-	$(hide) ln -sf egl/libGLESv2_adreno $@/libGLESv2_adreno
+	$(hide) ln -sf egl/libGLESv2_adreno.so $@/libGLESv2_adreno.so
 
 ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_MOUNT_POINT) $(FIRMWARE_MODEM_MOUNT_POINT) $(DSP_MOUNT_POINT)
 ALL_DEFAULT_INSTALLED_MODULES += $(RFS_MSM_ADSP_SYMLINKS) $(RFS_MSM_CDSP_SYMLINKS) $(RFS_MSM_MPSS_SYMLINKS) $(RFS_MSM_SLPI_SYMLINKS)
