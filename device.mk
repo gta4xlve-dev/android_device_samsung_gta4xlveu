e1344fffec0d6e91dc1e63b89983cc5297c1762c
danielml <daniel@danielml.dev>
2023-10-08 11:56:57 +0200

    Revert "gta4xlveu: Disable radio related settings on the WiFi-only model"
    * This also hides the per-app network settings, which include WiFi settings
      that are applicable for both models
    
    This reverts commit 8ba70bcc0fb3d927c26e78093a5a174defc22d3a.

--- device.mk
+++ device.mk
@@ -166,9 +166,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_PACKAGES += \
-    SamsungNoRilSettingsOverlay
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
