52fcabba2c34334dcf7150b72440fe45ac87162d
danielml <daniel@danielml.dev>
2023-11-15 14:07:32 +0100

    gta4xlveu: Switch to prebuilt camera provider
    * Due to a mismatch of camera.device constructors

--- device.mk
+++ device.mk
@@ -69,8 +69,8 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
-    android.hardware.camera.provider@2.5-service \
-    android.hardware.camera.provider@2.4-impl
+    android.hardware.camera.device@3.6.vendor \
+    android.hardware.camera.provider@2.6.vendor
 
 # Display
 PRODUCT_PACKAGES += \
