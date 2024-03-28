668a149b5a188c981c8925cdd77c49f33e38bfc1
Bruno Martins <bgcngm@gmail.com>
2024-03-28 13:11:44 +0000

    gta4xlveu: Declare EGL libs as symlinks during extraction
    Change-Id: I4b2f3e79b5e518889d9771bf8990e27babca8b61

--- Android.mk
+++ Android.mk
@@ -18,20 +18,6 @@ $(DSP_MOUNT_POINT): $(LOCAL_INSTALLED_MODULE)
 	@echo "Creating $(DSP_MOUNT_POINT)"
 	@mkdir -p $(TARGET_OUT_VENDOR)/dsp
 
-EGL_SYMLINKS := $(TARGET_OUT_VENDOR)/lib/
-$(EGL_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	mkdir -p $@
-	$(hide) ln -sf egl/libEGL_adreno.so $@/libEGL_adreno.so
-	$(hide) ln -sf egl/libGLESv2_adreno.so $@/libGLESv2_adreno.so
-
-
-EGL64_SYMLINKS := $(TARGET_OUT_VENDOR)/lib64/
-$(EGL64_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	mkdir -p $@
-	$(hide) ln -sf egl/libEGL_adreno.so $@/libEGL_adreno.so
-	$(hide) ln -sf egl/libGLESv2_adreno.so $@/libGLESv2_adreno.so
-
 ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_MOUNT_POINT) $(FIRMWARE_MODEM_MOUNT_POINT) $(DSP_MOUNT_POINT)
-ALL_DEFAULT_INSTALLED_MODULES += $(EGL_SYMLINKS) $(EGL64_SYMLINKS)
 
 endif
