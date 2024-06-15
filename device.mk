28cbfcab108563f174aa4e71c5f2f9cf48f97a97
Willi Ye <williye97@gmail.com>
2024-06-15 12:02:46 +0200

    gta4xlveu: Import Samsung Ssensor permissions
    Change-Id: I3d38205ca7ab3ee72a45750b6970dbb1e9e5befa

--- device.mk
+++ device.mk
@@ -215,6 +215,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_SM-P619/android.hardware.telephony.gsm.xml
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/com.samsung.permission.SSENSOR.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.samsung.permission.SSENSOR.xml
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.pixel-libperfmgr
