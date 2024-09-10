9e4e9dad4d754975705361a4446024b1cb031ef4
Michael Bestas <mkbestas@gmail.com>
2024-09-11 00:04:13 +0300

    gta4xlveu: Shim widevine with libcrypto_shim
    The following change
    https://github.com/google/boringssl/commit/e202e51cb0912f36dafbd2e67cf04d6ec82f3180
    removed symbols that our libs require.
    
    Change-Id: I1325c34ecec3d339dd68c665010ecb36fa2e6a29

--- extract-files.sh
+++ extract-files.sh
@@ -62,6 +62,10 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libcrypto.so" "libcrypto-v33.so" "${2}"
             ;;
+        vendor/lib/libwvhidl.so|vendor/lib/mediadrm/libwvdrmengine.so)
+            [ "$2" = "" ] && return 0
+            grep -q "libcrypto_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
+            ;;
         *)
             return 1
             ;;
