01dd90a82eeb0be2e491ff4c095ea89692c40016
danielml <daniel@danielml.dev>
2024-10-10 19:05:46 +0300

    gta4xlveu: Import wpa_supplicant_overlay.conf to the device tree
    Signed-off-by: danielml <daniel@danielml.dev>

--- device.mk
+++ device.mk
@@ -320,5 +320,8 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
+
 # Proprietary components
 $(call inherit-product, vendor/samsung/gta4xlveu/gta4xlveu-vendor.mk)
