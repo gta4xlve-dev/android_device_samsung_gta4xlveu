ba91ea995a725d2dd4c72a66bb13324fd4a32494
danielml <daniel@danielml.dev>
2024-10-13 11:25:26 +0200

    gta4xlveu: Shim libscaler_{hw,sw}.unifunc.so with libui_shim.so
    Change-Id: Ibcab15c66c02572f867e8234e5ecc41db0d6e628
    Signed-off-by: danielml <daniel@danielml.dev>

--- extract-files.sh
+++ extract-files.sh
@@ -66,6 +66,11 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             grep -q "libcrypto_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
             ;;
+        vendor/lib/libscaler_hw.unifunc.so|vendor/lib64/libscaler_hw.unifunc.so|\
+        vendor/lib/libscaler_sw.unifunc.so|vendor/lib64/libscaler_sw.unifunc.so)
+            [ "$2" = "" ] && return 0
+            grep -q "libui_shim.so" "${2}" || "${PATCHELF}" --add-needed "libui_shim.so" "${2}"
+            ;;
         *)
             return 1
             ;;
