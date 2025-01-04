8f08603aa32fdf79f1458c05aa9ad7cb2a3e79c5
Michael Bestas <mkbestas@lineageos.org>
2025-01-04 09:09:32 +0200

    gta4xlveu: Remove OMX audio codecs
     * OMX HW audio codecs won't work without QCOM A/V enhancements, which
       we don't have on Lineage.
    
    Change-Id: I940be6716b603e848c44839bb87ad690eb55e86f

--- device.mk
+++ device.mk
@@ -22,10 +22,7 @@ PRODUCT_PACKAGES += \
     liba2dpoffload \
     libexthwplugin \
     libhfp \
-    libbatterylistener \
-    libOmxAmrEnc \
-    libOmxAacEnc \
-    libOmxEvrcEnc
+    libbatterylistener
 
 PRODUCT_PACKAGES += \
     android.hardware.soundtrigger@2.0-impl \
@@ -152,7 +149,6 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_PACKAGES += \
-    libmm-omxcore \
     libOmxCore \
     libc2dcolorconvert \
     libOmxVdec \
