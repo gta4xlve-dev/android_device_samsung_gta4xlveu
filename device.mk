78e54fae15f576a27e6d92d7331927290a9de31a
Bruno Martins <bgcngm@gmail.com>
2025-06-16 21:04:22 +0200

    gta4xlveu: Migrate Lineage Health to soong_config_set
    Change-Id: I2eff39e83b9673aa2989d05de65c646241ba61da

--- device.mk
+++ device.mk
@@ -120,6 +120,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/batt_slate_mode)
+$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
+$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
+$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
+$(call soong_config_set,lineage_health,charging_control_supports_deadline,false)
+$(call soong_config_set,lineage_health,charging_control_supports_toggle,true)
+
 # HIDL
 PRODUCT_PACKAGES += \
     libhwbinder.vendor
