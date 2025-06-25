d3bbb309b66b860845cec081a01c0cc859e3275b
Tim Zimmermann <tim@linux4.de>
2025-06-25 21:11:09 +0200

    gta4xlveu: Uprev audio HAL to V7
    Change-Id: Iede32c176742e89577365341732a86f6ecaee514

--- device.mk
+++ device.mk
@@ -5,8 +5,8 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
-    android.hardware.audio@6.0-impl \
-    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio@7.0-impl \
+    android.hardware.audio.effect@7.0-impl \
     audio.primary.atoll \
     audio.r_submix.default \
     audio.usb.default \
