f65796d85210815020c556e3080caeae3c7cf149
danielml <daniel@danielml.es>
2025-12-29 13:33:54 +0100

    gta4xlveu: Build vendor.qti.hardware.camera.device@1.0 from source
    Signed-off-by: danielml <daniel@danielml.es>

--- extract-files.py
+++ extract-files.py
@@ -43,9 +43,6 @@ def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
 lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
     (
-        'vendor.qti.hardware.camera.device@1.0',
-    ): lib_fixup_vendor_suffix,
-    (
         'libc2dcolorconvert',
         'libplatformconfig',
         'libwpa_client',
