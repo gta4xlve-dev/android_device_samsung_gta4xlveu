c2ce8d6b15c1480c8f96859c92453dc7a853f592
dianlujitao <dianlujitao@lineageos.org>
2026-03-27 19:53:16 +0100

    gta4xlveu: Remove OMX
     * OMX HW video codecs are no longer used after enabling Codec2
     * OMX HW audio codecs won't work without QCOM A/V enhancements, which
       we don't have on Lineage.
    
    Change-Id: I65852151c8fb6387add5fc4402ba3f13c97b5f64

--- device.mk
+++ device.mk
@@ -155,18 +155,6 @@ PRODUCT_PACKAGES += \
     libcurl.vendor
 
 # Media
-PRODUCT_PACKAGES += \
-    libOmxCore \
-    libc2dcolorconvert \
-    libOmxVdec \
-    libOmxVenc \
-    libstagefrighthw \
-    libstagefright_omx
-
-PRODUCT_PACKAGES += \
-    libavservices_minijail.vendor \
-    libavservices_minijail_vendor
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
     $(LOCAL_PATH)/configs/media/media_codecs_performance_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_c2.xml
